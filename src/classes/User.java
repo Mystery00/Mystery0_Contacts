e85e7c7c2e27b18722077326b2092d1228d7f6e9
Mystery00 <mystery0dyl520@gmail.com>
2017-06-19 18:19:02 +0800

    file

--- src/classes/User.java
+++ src/classes/User.java
@@ -4,7 +4,6 @@ public class User
 {
     private String username;
     private String password;
-    private String userType;
 
     public String getUsername()
     {
@@ -24,15 +23,5 @@ public class User
     public void setPassword(String password)
     {
         this.password = password;
-    }
-
-    public String getUserType()
-    {
-        return userType;
-    }
-
-    public void setUserType(String userType)
-    {
-        this.userType = userType;
     }
 }
